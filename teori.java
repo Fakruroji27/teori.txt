7a82cb70e656f4f41cccd7d34e193ac4b06188a9
Fakruroji27 <103887129+Fakruroji27@users.noreply.github.com>
2022-04-18 15:50:08 +0700

    Add files via upload

---
+++ teori.java
@@ -0,0 +1,7 @@
+package oji;
+
+public class teori {Matriks ialah struktur penyimpanan data pada memori utama yang individu elemennya diacu dengan memakai 2 buah indeks
+    yang sering dikonotasikan dengan baris dan kolom
+    hubungan metrix dengan array yaitu pada elemmenya yang memiliki tipe yang sama dan semua elemennya bisa diakses
+    dengan langsung 
+}
